474ff7a58872eb2db40c382c762c64fa9f60445e
sonu1680 <sonupandit1680@gmail.com>
2025-07-11 23:38:21 +0530

    fibonaci

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 fn main() {
-println!("{}",is_even(-21));
+println!("{}",fibonaic(4));
 
 }
 
 
-fn is_even(num:u8)->bool{
-    if num%2==0 {
-        return true;
-    }
-    else{
+// fn is_even(num:i8)->bool{
+//     if num%2==0 {
+//         return true;
+//     }
+//     else{
 
-        return false;
-    }
+//         return false;
+//     }
+// }
+
+// fn fibonaic(num:i8)->i8{
+ let mut first:i8=0;
+ let mut second:i8=1;
+
+ if num==0{
+    return first;
+ }
+
+ if num==1{
+    return second;
+ }
+ for _ in 0..(num-1){
+     let  temp=second;
+     //println!("{}",temp);
+     second=second+first;
+     first=temp;
+}
+return second;
+ 
+
 }
